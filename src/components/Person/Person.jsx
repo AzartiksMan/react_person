758d5618422ab0d234896676b195a93a8de69507
Denis Kasatskiy <azartofgod@gmail.com>
2025-05-13 20:40:20 +0300

    Fix

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -7,7 +7,7 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
 
-      {age && <p className="Person__age">{`I am ${age}`}</p>}
+      {age !== undefined && <p className="Person__age">{`I am ${age}`}</p>}
 
       {isMarried === false ? (
         <p className="Person__partner">I am not married</p>
